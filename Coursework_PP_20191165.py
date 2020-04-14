9c9a3f61e79b41d17a529b5d27edcc569d785a5b
lakshanKL <kavilja99@gmail.com>
2020-04-14 22:51:36 +0530

    Validation is added

--- Coursework_PP_20191165.py
+++ Coursework_PP_20191165.py
@@ -5,72 +5,76 @@ exit=1
 if exit==1:
     while exit==1:
         id= input('Enter your Student ID: ')
-        p= int(input('Ener your Passed credits: '))
-        d= int(input('Enter your defered credits: '))
-        f= int(input('Enter your failed credits: '))
-
-            if ((p and d and f)>=0):
-                if (p+d+f)==120:
-                    if (p==120 and d==0 and f==0):
-                        print ('Progress')
-                    elif (p==100 and d==20 and f==0):
-                        print ('Progress module-trailer')
-                    elif (p==100 and d==0 and f==20):
-                        print ('Progress module-trailer')
-                    elif (p==80 and d==40 and f==0):
-                        print ('Do not Progress – module retriever')
-                    elif (p==80 and d==20 and f==20):
-                        print ('Do not Progress – module retriever')
-                    elif (p==80 and d==0 and f==40):
-                        print ('Do not Progress – module retriever')
-                    elif (p==60 and d==60 and f==0):
-                        print ('Do not Progress – module retriever')
-                    elif (p==60 and d==40 and f==20):
-                        print ('Do not Progress – module retriever')
-                    elif (p==60 and d==20 and f==40):
-                        print ('Do not Progress – module retriever')
-                    elif (p==60 and d==0 and f==60):
-                        print ('Do not Progress – module retriever')
-                    elif (p==40 and d==80 and f==0):
-                        print ('Do not Progress – module retriever')
-                    elif (p==40 and d==60 and f==20):
-                        print ('Do not Progress – module retriever')
-                    elif (p==40 and d==40 and f==40):
-                        print ('Do not Progress – module retriever')
-                    elif (p==40 and d==20 and f==60):
-                        print ('Do not Progress – module retriever')
-                    elif (p==40 and d==0 and f==80):
-                        print ('Exclude')
-                    elif (p==20 and d==100 and f==0):
-                        print ('Do not Progress – module retriever')
-                    elif (p==20 and d==80 and f==20):
-                        print ('Do not Progress – module retriever')
-                    elif (p==20 and d==60 and f==40):
-                        print ('Do not Progress – module retriever')
-                    elif (p==20 and d==40 and f==60):
-                        print ('Do not Progress – module retriever')
-                    elif (p==20 and d==20 and f==80):
-                        print ('Exclude')
-                    elif (p==20 and d==0 and f==100):
-                        print ('Exclude')
-                    elif (p==0 and d==120 and f==0):
-                        print ('Do not Progress – module retriever')
-                    elif (p==0 and d==100 and f==20):
-                        print ('Do not Progress – module retriever')
-                    elif (p==0 and d==80 and f==40):
-                        print ('Do not Progress – module retriever')
-                    elif (p==0 and d==60 and f==60):
-                        print ('Do not Progress – module retriever')
-                    elif (p==0 and d==40 and f==80):
-                        print ('Exclude')
-                    elif (p==0 and d==20 and f==100):
-                        print ('Exclude')
-                    elif (p==0 and d==0 and f==120):
-                        print ('Exclude')
-                    else:
-                        print ('Range error')
+        try:
+            p = int(input('PASSED CREDITS: ') or "0")
+            d = int(input('DEFERED CREDITS: ') or "0")
+            f = int(input('FAILED CREDITS: ') or "0")
+            
+            if (p+d+f)==120:
+                exit=0
+                if (p==120 and d==0 and f==0):
+                    print ('Student ID:',id,'\n','Progress')
+                elif (p==100 and d==20 and f==0):
+                    print ('Student ID:',id,'\n','Progress module-trailer')
+                elif (p==100 and d==0 and f==20):
+                    print ('Student ID:',id,'\n','Progress module-trailer')
+                elif (p==80 and d==40 and f==0):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==80 and d==20 and f==20):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==80 and d==0 and f==40):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==60 and d==60 and f==0):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==60 and d==40 and f==20):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==60 and d==20 and f==40):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==60 and d==0 and f==60):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==40 and d==80 and f==0):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==40 and d==60 and f==20):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==40 and d==40 and f==40):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==40 and d==20 and f==60):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==40 and d==0 and f==80):
+                    print ('Student ID:',id,'\n','Exclude')
+                elif (p==20 and d==100 and f==0):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==20 and d==80 and f==20):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==20 and d==60 and f==40):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==20 and d==40 and f==60):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==20 and d==20 and f==80):
+                    print ('Student ID:',id,'\n','Exclude')
+                elif (p==20 and d==0 and f==100):
+                    print ('Student ID:',id,'\n','Exclude')
+                elif (p==0 and d==120 and f==0):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==0 and d==100 and f==20):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==0 and d==80 and f==40):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==0 and d==60 and f==60):
+                    print ('Student ID:',id,'\n','Do not Progress – module retriever')
+                elif (p==0 and d==40 and f==80):
+                    print ('Student ID:',id,'\n','Exclude')
+                elif (p==0 and d==20 and f==100):
+                    print ('Student ID:',id,'\n','Exclude')
+                elif (p==0 and d==0 and f==120):
+                    print ('Student ID:',id,'\n','Exclude')
                 else:
-                    print ('Total incorrect')
+                    print ('Range error')
+            else:
+                print ('Total incorrect')
+        except:
+            print ('Integeer Required')
+        
                 
                         
                         
